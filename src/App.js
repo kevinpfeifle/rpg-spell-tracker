0469b2d31456baf3a4344443723171bc4afd8a49
Kevin Pfeifle <kevinpfeifle@outlook.com>
2021-02-05 09:33:36 -0500

    made app into class component, renders loading while waiting for DB

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // React imports
-import { useState, useEffect, useCallback } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactLogo from './d20.png';
 
 // DB imports
 import {fetchSpell, fetchSpells} from './database/db';
@@ -14,48 +15,72 @@ import Spellbook from './components/Spellbook/Spellbook';
 // Util imports
 import {spellSorting} from './utils/spellTransforms';
 
-function App() {
-  const [pureSpells, setPureSpells] = useState([]); // An unchanging state reference to the original spell list. Needed as filter mutates the spells list.
-  const [spells, setSpells] = useState([]);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pureSpells: [],
+      spells: []
+    }
+  }
+
+  componentDidMount() {
+    this.getSpells().then((spells) => {
+      this.setState({
+        ...this.state,
+        pureSpells: spells,
+        spells: spells
+      });
+    });
+  }
 
   // GETs the spell list and sorts before setting it to state.
-  useEffect(() => {
-    const getSpells = async () => {
-      const spellsFromServer = await fetchSpells();
-      setSpells(spellSorting(spellsFromServer));
-      setPureSpells(spellSorting(spellsFromServer));
-    };
-    getSpells();
-  }, []);
-
-  // Callback function to be executed by Spellbook Header component on rerender of state.
-  const sortSpells = useCallback((spells, sortField, sortDirection) => {
-    setSpells(spellSorting(spells, sortField, sortDirection));
-  }, []);
-
-  const filterSpells = (spells) => {
-    setSpells(spells);
+   getSpells = () => {
+     return new Promise(async (resolve, reject) => {
+       const spellsFromServer = await fetchSpells();
+       resolve(spellSorting(spellsFromServer));
+      });
   };
 
-  return (
-    <Router>
-      <div>
-        <Route path='/' exact render={(props) => (
-          <>          
-            <UnderConstruction />
-          </>
-        )} />
-        <Route 
-          path='/sample-spellbook' 
-          render={() => (
-            <Spellbook spells={spells} pureSpells={pureSpells} sorting={sortSpells} filter={filterSpells}/>
-          )} 
-        /> 
-        <Route path='/about' component={About} /> 
-        <Footer />
-      </div>
-    </Router>
-  );
+  // Callback function to be executed by Spellbook Header component on rerender of state.
+  sortSpells = (spells, sortField, sortDirection) => {
+    this.setState({
+      ...this.state,
+      spells: spellSorting(spells, sortField, sortDirection)
+    });
+  };
+
+  filterSpells = (spells) => {
+    this.setState({
+      ...this.state,
+      spells: spells
+    });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path='/' exact render={(props) => (
+            <>          
+              <UnderConstruction />
+            </>
+          )} />
+          <Route 
+            path='/sample-spellbook' 
+            render={() => {
+              // To ensure the spellbook as spell props on render, show a spinning loading image until all least one spell is ready for display.
+              return (this.state.spells.length > 0) ?
+                (<Spellbook spells={this.state.spells} pureSpells={this.state.pureSpells} sorting={this.sortSpells} filter={this.filterSpells}/>)
+                : (<img className='Loading' src={ReactLogo} alt='Loading...' style={{width:'250px', height:'250px'}}/>)
+            }} 
+          /> 
+          <Route path='/about' component={About} /> 
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
