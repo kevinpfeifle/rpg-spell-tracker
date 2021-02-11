0a0a14ff9b883918415b75269d874df74e14f410
Kevin Pfeifle <kevinpfeifle@outlook.com>
2021-02-11 08:30:25 -0500

    added cors configurations to backend for localhost view

--- server/index.js
+++ server/index.js
@@ -5,11 +5,17 @@ const db = require('./database/db');
 
 // External imports for the server.
 const config = require('config');
+const cors = require('cors');
 const express = require('express');
 const app = express();
   
-// Sets up the server to listen on the port with DB access.
+// Sets up the server to listen on the port with CORS access for the localhost.
+let corsOptions = {
+    origin: config.get('client.host') + ':' + config.get('client.port'),
+    optionsSuccessStatus: 200
+};
 let port = config.get('expressServer.port');
+app.use(cors(corsOptions));
 app.listen(port, () => {
     db.init(config.get('dbCredentials'));
     console.log(`RPG Tool Server running on port: ${port}`);
