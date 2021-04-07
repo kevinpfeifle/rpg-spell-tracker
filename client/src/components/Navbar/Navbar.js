a9ee61d8e44d51fb8479da02a3b610e53122c09d
Kevin Pfeifle <kevinpfeifle@outlook.com>
2021-04-07 14:52:42 -0400

    added login button to the navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,8 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
-// Vendor imports.
+// Asset imports.
+import LoginIcon from '../../assets/userlogo.png';
 import LoadingIcon from '../../vendor/d20.png';
 
 class Navbar extends React.Component {
@@ -10,10 +11,15 @@ class Navbar extends React.Component {
         return (
             <div className='navbar'>
                 <ul>
-                    <li><NavLink exact to='/'><img className='RPGToolIcon' src={LoadingIcon} alt='RPGToolICon' style={{width:'50px',height:'50px'}}/></NavLink></li>
-                    <li><NavLink exact to='/sample-spellbook'>Spellbook</NavLink></li>
-                    <li><NavLink exact to='/spell-compendium'>Spell Compendium</NavLink></li>
-                    <li><NavLink exact to='/about'>About</NavLink></li>
+                    <div className='navbarLeft'>
+                        <li><NavLink exact to='/'><img className='RPGToolIcon' src={LoadingIcon} alt='RPGToolICon' style={{width:'50px',height:'50px'}}/></NavLink></li>
+                        <li><NavLink exact to='/sample-spellbook'>Spellbook</NavLink></li>
+                        <li><NavLink exact to='/spell-compendium'>Spell Compendium</NavLink></li>
+                        <li><NavLink exact to='/about'>About</NavLink></li>
+                    </div>
+                    <div className='navbarRight'>
+                        <li id='loginIcon'><NavLink exact to='/login'><img className='LoginIcon' src={LoginIcon} alt='LoginIcon' style={{width:'40px',height:'40px'}}/></NavLink></li>
+                    </div>
                 </ul>
             </div>
         )
